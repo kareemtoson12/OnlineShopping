efb2e52cb21485c2eff6439fd2afff41c28d62b6
kareemtoson <kareemtoson1@gmail.com>
2024-11-25 13:19:42 +0200

    git hub linking done

---
+++ online_shopping/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:online_shopping/online_shopping.dart';
+
+void main() {
+  runApp(const OnlineShopping());
+}
---
+++ online_shopping/lib/online_shopping.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OnlineShopping extends StatelessWidget {
+  const OnlineShopping({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Online shopping',
+      home: null,
+    );
+  }
+}
