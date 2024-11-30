65f9b384b6792b4ba9ef8a54c84a44cdf7ec6066
kareemtoson <kareemtoson1@gmail.com>
2024-11-30 11:02:37 +0200

    sign in / login screen done

--- online_shopping/lib/features/login/widgets/login_button.dart
+++ online_shopping/lib/features/login/widgets/login_button.dart
@@ -1,9 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:online_shopping/core/routing/app_routes.dart';
-import 'package:online_shopping/core/services/auth_service.dart';
+
 import 'package:online_shopping/core/styles/customs_colors.dart';
 import 'package:online_shopping/core/styles/styles.dart';
 
