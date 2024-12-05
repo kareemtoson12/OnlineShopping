f5745faad154095b407465f088c6f04077a5a810
kareemtoson <kareemtoson1@gmail.com>
2024-12-05 14:11:58 +0200

    Admin add delete edit

--- online_shopping/lib/features/adminPanel/editProducts/view.dart
+++ online_shopping/lib/features/adminPanel/editProducts/view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:online_shopping/core/routing/app_routes.dart';
 import 'package:online_shopping/core/styles/customs_colors.dart';
