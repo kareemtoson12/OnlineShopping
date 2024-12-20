a82df3380b6bae64831da78bf080ccca74914fe1
Marina <98766777+Marina64-64@users.noreply.github.com>
2024-12-21 00:27:45 +0200

    Update EmailandPasswordForSignUp.dart

--- online_shopping/lib/features/signUp/widgets/EmailandPasswordForSignUp.dart
+++ online_shopping/lib/features/signUp/widgets/EmailandPasswordForSignUp.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:online_shopping/core/routing/app_routes.dart';
 import 'package:online_shopping/core/styles/customs_colors.dart';
-
 import 'package:online_shopping/core/styles/styles.dart';
-
 import 'package:online_shopping/features/signUp/widgets/signUp_buttton.dart';
 
+// Widget for email and password form during the sign-up process
 class EmailandPasswordForSignUp extends StatefulWidget {
   const EmailandPasswordForSignUp({super.key});
 
@@ -16,41 +15,40 @@ class EmailandPasswordForSignUp extends StatefulWidget {
 }
 
 class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
+  // Form key to manage form state and validations
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  // Controllers for managing form input fields
   final TextEditingController _emailController = TextEditingController();
-
   final TextEditingController _firstNameController = TextEditingController();
-
   final TextEditingController _LastNameController = TextEditingController();
-
   final TextEditingController _userNameController = TextEditingController();
-
   final TextEditingController _phoneNumberController = TextEditingController();
-
   final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Form(
-      key: _formKey,
+      key: _formKey, // Attach the form key for validation purposes
       child: Column(
         children: [
+          // Row to handle first and last name fields side by side
           Row(
             children: [
+              // First Name Input Field
               Expanded(
                 child: TextFormField(
                   controller: _firstNameController,
                   decoration: InputDecoration(
-                    labelText: 'First Name',
+                    labelText: 'First Name', // Field label
                     prefixIcon: const Icon(
-                      Icons.person,
+                      Icons.person, // Icon for input field
                       color: CustomsColros.primaryColor,
                     ),
                     labelStyle: AppTextStyles.fontForLabel,
                     enabledBorder: OutlineInputBorder(
                       borderSide: const BorderSide(
-                          color: CustomsColros.gray, width: 1.4),
+                          color: CustomsColros.gray, width: 1.4), // Border style
                       borderRadius: BorderRadius.circular(20.0),
                     ),
                     border: OutlineInputBorder(
@@ -58,6 +56,7 @@ class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
                     ),
                   ),
                   validator: (value) {
+                    // Validate the field to ensure it is not empty
                     if (value == null || value.isEmpty) {
                       return 'Please enter your first name';
                     }
@@ -65,7 +64,8 @@ class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
                   },
                 ),
               ),
-              SizedBox(width: 10.w), // Add spacing between the fields
+              SizedBox(width: 10.w), // Spacing between fields
+              // Last Name Input Field
               Expanded(
                 child: TextFormField(
                   controller: _LastNameController,
@@ -95,14 +95,15 @@ class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
               ),
             ],
           ),
-          SizedBox(height: 20.h),
+          SizedBox(height: 20.h), // Spacing after row
+          // Username Input Field
           TextFormField(
             controller: _userNameController,
             decoration: InputDecoration(
               labelText: 'UserName',
               prefixIcon: const Icon(
                 Icons.person,
-                color: CustomsColros.primaryColor, // Corrected name
+                color: CustomsColros.primaryColor,
               ),
               labelStyle: AppTextStyles.fontForLabel,
               enabledBorder: OutlineInputBorder(
@@ -114,7 +115,7 @@ class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
                 borderRadius: BorderRadius.circular(20.r),
               ),
             ),
-            keyboardType: TextInputType.emailAddress,
+            keyboardType: TextInputType.text,
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return 'Please enter your userName';
@@ -123,9 +124,9 @@ class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
             },
           ),
           SizedBox(height: 20.h),
+          // Phone Number Input Field
           TextFormField(
             controller: _phoneNumberController,
-            obscureText: true,
             decoration: InputDecoration(
               prefixIcon: const Icon(
                 Icons.phone,
@@ -141,22 +142,20 @@ class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
             ),
             validator: (value) {
               if (value == null || value.isEmpty) {
-                return 'Please enter your password';
-              }
-              if (value.length < 6) {
-                return 'Password must be at least 6 characters long';
+                return 'Please enter your phone number';
               }
               return null;
             },
           ),
           SizedBox(height: 20.h),
+          // Email Input Field
           TextFormField(
             controller: _emailController,
             decoration: InputDecoration(
               labelText: 'Email',
               prefixIcon: const Icon(
                 Icons.email,
-                color: CustomsColros.primaryColor, // Corrected name
+                color: CustomsColros.primaryColor,
               ),
               labelStyle: AppTextStyles.fontForLabel,
               enabledBorder: OutlineInputBorder(
@@ -177,9 +176,10 @@ class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
             },
           ),
           SizedBox(height: 20.h),
+          // Password Input Field
           TextFormField(
             controller: _passwordController,
-            obscureText: true,
+            obscureText: true, // Hide the text for password fields
             decoration: InputDecoration(
               prefixIcon: const Icon(
                 Icons.lock,
@@ -204,9 +204,7 @@ class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
             },
           ),
           SizedBox(height: 20.h),
-
-          SizedBox(height: 10.h),
-          // Login button
+          // Sign-Up Button
           SignUpButton(
             emailController: _emailController,
             firstNameController: _firstNameController,
@@ -216,6 +214,7 @@ class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
             userNameController: _userNameController,
           ),
           SizedBox(height: 10.h),
+          // Navigate to Login Screen
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -224,7 +223,7 @@ class _EmailandPasswordForSignUpState extends State<EmailandPasswordForSignUp> {
                   Navigator.pushReplacementNamed(context, Routes.login);
                 },
                 child: Text(
-                  'Already have an account? Login now !',
+                  'Already have an account? Login now!',
                   style: AppTextStyles.fontForLabel,
                 ),
               )
